88cdf4bd6666d8e27f7180e460ad96db21ff7691
Dave Katten <doc@simpli.fi>
2019-11-14 10:24:13 -0600

    Match screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,16 @@ class HomeController < ApplicationController
     # @planet = Planet.order('RANDOM()').first
     @name = params[:name] || random_string
     @composition = Compositor.new(@name).composite
+    render is_match? ? 'match' : 'index'
   end
+
+  private
 
   def random_string
     (0...8).map { (65 + rand(26)).chr }.join
+  end
+
+  def is_match?
+    params[:match] && rand(10) < 3
   end
 end
